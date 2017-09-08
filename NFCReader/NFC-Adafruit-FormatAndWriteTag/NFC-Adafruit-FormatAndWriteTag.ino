4d2204d815f764b654051a2d1208aeeb203dc5bc
Dudley smith <vj.dudley.smith@gmail.com>
2017-09-08 11:59:47 +0200

    Refactor pour sortir le test de présence jusqu'au niveau du sketch

--- NFCReader/NFC-Adafruit-FormatAndWriteTag/NFC-Adafruit-FormatAndWriteTag.ino
+++ NFCReader/NFC-Adafruit-FormatAndWriteTag/NFC-Adafruit-FormatAndWriteTag.ino
@@ -47,24 +47,32 @@ void loop() {
     // --
   } while (readChar != '\n');
 
-  // 1/ FORMAT
-  Serial.println("[ENTER] , now we format and write into the Tag :");
-  Serial.print("Sentence written : ");
-  Serial.println(readString);
-  NfcWrapper.formatMifare();
+  if (NfcWrapper.isTagPresent()) {
+    
+    // 1/ FORMAT
+    Serial.println("[ENTER] , now we format and write into the Tag :");
+    Serial.print("Sentence written : ");
+    Serial.println(readString);
+    NfcWrapper.formatMifare();
 
-  // 2/ WRITE
-  NfcWrapper.writeMifareBlock(4, readString);
+    // 2/ WRITE
+    NfcWrapper.writeMifareBlock(4, readString);
 
-  // 3/ READ CHECK
-  String valueString = NfcWrapper.readMifareBlock(4);
-  Serial.print("Sentence read : ");
-  Serial.println(valueString);
-  // Wait a bit before trying again
-  Serial.println("Done!");
-  Serial.println("-----------------------------------------");
-  Serial.println();
-  Serial.println();
+    // 3/ READ CHECK
+    String valueString = NfcWrapper.readMifareBlock(4);
+    Serial.print("Sentence read : ");
+    Serial.println(valueString);
+    
+    // Wait a bit before trying again
+    Serial.println("Done!");
+    Serial.println("-----------------------------------------");
+    Serial.println();
+    Serial.println();
+
+  } else {
+    Serial.println("Waiting for a tag ..............");
+  }
+
   delay(1000);
 
   // Then Flush
